771d913fb67dfd8c4a2e1c8494a41beb591d89ea
Marc Cooke-Russell <marc.cookerussell@gmail.com>
2020-04-05 12:17:40 +1100

    Renderer 2D uses a single shader now

--- Acrylic/src/Acrylic/Renderer/Texture.cpp
+++ Acrylic/src/Acrylic/Renderer/Texture.cpp
@@ -6,6 +6,18 @@
 
 namespace Acrylic
 {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:    AC_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLTexture2D>(width, height);
+		}
+
+		AC_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
